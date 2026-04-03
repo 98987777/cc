504262767b78a1d19e9a4f0ae60e5f7441567ca2
Ashlesha Nanaware <102414690+98987777@users.noreply.github.com>
2026-04-03 23:37:39 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -14,12 +14,12 @@ Steps: -
 5.	For guest OS choose VMWare ESX and for version select 5.0 x
 6.	Then Save the VM in your choice folder
 7.	Keep the number of processors as it is
-8.	Memory for the VM changed to 6000
+8.	Memory for the VM changed to 8000
 9.	In network connection type keep it as it is
 10.	 In I/O Controller types keep it as it is
 11.	 Virtual disk type same as it is 
 12.	 In selecting Virtual disk keep as it is
-13.	 Specific Disk Capacity as it is and choose store virtual disk as a single file 
+13.	 Make the Specific Disk Capacity as 200 and choose store virtual disk as a single file 
 14.	 At specify disk file browse to your chosen folder and click next
 15.	 Now on left side click on edit Virtual machine settings
 16.	 In Hardware > CD/DVD in connection select Use ISO image file
@@ -32,7 +32,7 @@ Steps: -
 23.	 OR  when Making a Virtual Machine in Workstation Pro in Specify Disk Capacity change 40 to 60. 
 
 
-24.	 After installation, Login using the VM IP Address (192.168.190.128) and username (root) and password (1234567)
+24.	After installation, Login using the VM IP Address (192.168.190.128) and username (root) and password (1234567)
 25.	Press Ignore. The VMware Evaluation Notice alert displays
 26.	After opening the vSphere Client click on Inventory icon
 27.	Make sure the virtual machine is started
@@ -46,7 +46,7 @@ Microsoft Windows 7 (64-bit).
 34.	Then next and again next and again next and again next 
 35.	SCSI Controller choose LSI Logic SAS
 36.	Select Create new virtual disk
-37.	Then in disk size keep 16 GB
+37.	Then in disk size keep 40 GB if size is bigger keep 16 only
 38.	Then next and finish
 39.	Then in Summary section under Storage right click on datastore1 and click Browse Datastore
 40.	In that create a new folder   named IOS Files
@@ -62,5 +62,6 @@ Microsoft Windows 7 (64-bit).
 49.	And Now Power On the VM and go to console tab.
 50.	 Now select Language, time and curreny and then ok .
 51.	 Then click on Install Now
-52.	 If there comes an error in the client machine while turning it on then go to windows+r > optionalfeatures> uncheck Hyper-v, Windows Hypervisor platform, virtual machine platform, windows, windows subsystem for linux
-Also check the first and third option in the dit settings > processors in the workstation 
+52.	 If there comes an error in the client machine while turning it on then go to windows+r > optionalfeatures> uncheck Hyper-v,windows hypervsor platform, virtual machine platform, windows sandbox, windows subsystem for linux
+Also check the first and third option in the edit settings > processors in the workstation 
+
