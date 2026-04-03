0ae9e0020e3cc6cbd43adc38cbaad86f97910197
Ashlesha Nanaware <102414690+98987777@users.noreply.github.com>
2026-04-04 00:46:01 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,31 +1,79 @@
+This Document provides the info about the things there in repo 
+Repo also includes the word documents names as CC & WS Practical 2,3,5,6,7,8,9 and p10.docx
+also has python code for copying text named as import
+
+                    
+=======================================================================
+p1:Implementing IaaS using VMWare ESXi Server
+
+Q. Implement IaaS using VMWare ESXi Server. 20 M
+
+
+
+=======================================================================
+
+p2:Implementation of Infrastructure as a Service Using Citrix Xen Server
+
+Q. Implement Infrastructure as a Service Using Citrix Xen Server. 20 M
+
+
+
+=======================================================================
+p3:Implementation of Infrastructure as a Service using Windows HyperV
+(Windows Server 2012 R2)
+
+Q. Demonstrate the implementation of Infrastructure as a Service using Windows
+Hyper V (Windows Server 2012 R2) 20 M
+
+
+=======================================================================
 p5a: convert temperature from Celsius to Fahrenheit and Fahrenheit to Celsiusp
 p5b: fibo, prime, odd/eve, multiplcation
 
 Q. Develop and consume a simple .NET web service for temperature conversion.
+
+
+
 =======================================================================
 p6: Creating and Running a Python Web Service using flask
 
 fibonacci, odd/even, prime number, largest of 3 number, addition
+
+
+
+
 =======================================================================
 p7: Implementation of GET and POST Methods using Flask
 
 Fibonacci, prime number, even odd, multiplication
+
+
+
+
 =======================================================================
 p8: Client–Server Communication Using ASP.NET Core MVC and Python Requests
 
 Q. Develop a temperature conversion web service in .NET and consume it from
 Python to demonstrate cross-platform web service integration.
+
+
+
+
 =======================================================================
-p9: Cross-platform Communication
+p9a: Cross-platform Communication
 p9b: Cross-platform Communication(Employee)
 
 Q. Develop a simple employee details web service in Python, such as adding and
 displaying employee records and consume it from a .NET application to
 demonstrate cross-platform web service integration.
+
+
+
 =======================================================================
 p10: Developing and deploying web service in Java
 
 Q. Implement and consume a Hello World Service in java
 A service that accepts a name and returns a greeting message. 15 M
+
 Q. Implement and consume a Square and Cube Service in java
 Accept a number and return its square and cube.
